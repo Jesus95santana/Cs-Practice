3c06e2d83475c3ebd1dd7b5d858ef0d34ad1daa0
jesus95santana <jesus95santana@yahoo.com>
2023-08-07 08:46:34 -0400

    Learned Data types & Variables

--- practice.cs
+++ practice.cs
@@ -1,14 +1,26 @@
 using System;
 
-namespace GettingInput
+namespace FavoriteNumber
 {
   class Program
   {
-    static void Main()
+    static void Main(string[] args)
     {
-      Console.WriteLine("How old are you?");
-      string input = Console.ReadLine();
-      Console.WriteLine($"You are {input} years old!");
+      // Ask user for fave number
+      Console.Write("Enter your favorite number!: ");
+
+      // Turn that answer into an int
+
+      // Attempt 1: use implicit conversion
+      // int faveNumber = Console.ReadLine();
+
+      // Attempt 2: use explicit conversion
+      // int faveNumber = (int)Console.ReadLine();
+
+      // Attempt 3: use Convert method
+      int faveNumber = Convert.ToInt32(Console.ReadLine());
+
+
     }
   }
 }
